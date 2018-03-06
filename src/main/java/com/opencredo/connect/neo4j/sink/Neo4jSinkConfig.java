128517bdb195b2202c51268aefb8b1fd41921631
Pegerto Fernandez <pegerto@gmail.com>
2018-03-06 00:00:14 +0000

    Uber jar and config

--- src/main/java/com/opencredo/connect/neo4j/sink/Neo4jSinkConfig.java
+++ src/main/java/com/opencredo/connect/neo4j/sink/Neo4jSinkConfig.java
@@ -2,22 +2,27 @@ package com.opencredo.connect.neo4j.sink;
 
 
 import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.Config;
 import org.apache.kafka.common.config.ConfigDef;
 
 import java.util.Map;
 
 public class Neo4jSinkConfig extends AbstractConfig{
 
+    public static final String BOLT_URL = "bolt.url";
+    private static final String BOLT_URL_DOC = "Bolt connection URL.";
+    private static final String BOLT_URL_DISPLAY = "Bolt URL";
 
-    public Neo4jSinkConfig(ConfigDef definition, Map<?, ?> originals, boolean doLog) {
-        super(definition, originals, doLog);
+    public Neo4jSinkConfig(Map<?, ?> props){
+        super(CONFIG_DEF, props);
     }
 
-    public Neo4jSinkConfig(ConfigDef definition, Map<?, ?> originals) {
-        super(definition, originals);
-    }
+    private static final String CONNECTION_GROUP = "Connection";
 
-    public static final ConfigDef CONFIG_DEF = new ConfigDef();
+
+    public static final ConfigDef CONFIG_DEF = new ConfigDef()
+            .define(BOLT_URL, ConfigDef.Type.STRING, ConfigDef.NO_DEFAULT_VALUE,
+                    ConfigDef.Importance.HIGH, BOLT_URL_DOC, CONNECTION_GROUP, 1, ConfigDef.Width.LONG,BOLT_URL_DISPLAY);
 
 
 }
--- src/test/java/com/opencredo/connect/neo4j/Neo4jSinkConnectorTest.java
+++ src/test/java/com/opencredo/connect/neo4j/Neo4jSinkConnectorTest.java
@@ -9,6 +9,9 @@ import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.text.MatchesPattern.matchesPattern;
 import static  org.hamcrest.Matchers.notNullValue;
@@ -43,6 +46,13 @@ public class Neo4jSinkConnectorTest extends TestCase {
     public void checkConfigDef(){
         ConfigDef conf = connector.config();
         assertThat(conf, notNullValue());
+    }
+
+    @Test
+    public void validateConfig(){
+        final Map<String, String> connectorConfigs = new HashMap<>();
+        connector.validate(connectorConfigs).configValues();
+
     }
 
 }
